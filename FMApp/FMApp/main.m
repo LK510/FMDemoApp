fa99547d4b0b57dbc6f715903498cfa701ef7cdd
Your Name Comes Here <lufei@anybeen.net>
2017-02-26 16:06:56 +0800

    app start

---
+++ FMApp/FMApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FMApp
+//
+//  Created by 叶凤鸣 on 17/2/26.
+//  Copyright © 2017年 yezi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
